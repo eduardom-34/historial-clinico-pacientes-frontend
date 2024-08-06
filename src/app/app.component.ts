eae19d66ada18afe5bc9bbb219c0cfd4d5a2bddc
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-05 21:58:27 -0600

    Solicitudes http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ValueChangeEvent } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titulo: string = 'Bienvenido a DoctorApp';
+  usuarios: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+     this.http.get("http://localhost:5223/api/usuario").subscribe({
+      next: response => this.usuarios = response,
+      error: error => console.log(error),
+      complete: () => console.log("La solicitud esta complete")
+     })
+  }
+
 }
