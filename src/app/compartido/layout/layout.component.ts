1d2817e329261d581a529712b4b4e1538d901fc7
eduardom-34 <eduardomelchor34@gmail.com>
2024-08-11 14:32:05 -0600

    Mostrar usuario conectado y cerrar Sesion

--- src/app/compartido/layout/layout.component.ts
+++ src/app/compartido/layout/layout.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { CompartidoService } from './../compartido.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+
+  username: string = '';
+
+  constructor( private router: Router, private compartidoService: CompartidoService ) {
+
+  }
+
+  ngOnInit(): void {
+    const usuarioToken = this.compartidoService.obtenerSesion();
+    if( usuarioToken != null ){
+      this.username = usuarioToken.username;
+    }
+  }
+
+  cerrarSesion(){
+    this.compartidoService.eliminarSesion();
+    this.router.navigate(['login']);
+  }
 
 }
