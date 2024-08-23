cffe2603cb4a2f17c9a039620b086dbc4c9bf737
eduardom-34 <eduardomelchor34@gmail.com>
2024-08-22 19:28:48 -0600

    Modificar Auth Guard para validacion del JWT

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,16 +1,28 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { CompartidoService } from '../compartido/compartido.service';
+import jwt_decode, { jwtDecode } from 'jwt-decode';
+import { CookieService } from 'ngx-cookie-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const compartidoServicio = inject(CompartidoService);
   const router = inject(Router);
+  const cookieService = inject(CookieService);
+  const usuario = compartidoServicio.obtenerSesion();
+  let token = cookieService.get("Authorization");
 
-  const usuarioToken = compartidoServicio.obtenerSesion();
-  if( usuarioToken != null) {
+  if (token && usuario) {
+    token = token.replace('Bearer ', '');
+    const decodeToken: any = jwtDecode(token);
+    const fechaExpiracion = decodeToken.exp * 1000;
+    const fechaActual = new Date().getTime();
+    if (fechaExpiracion < fechaActual) {
+      router.navigate(['login']);
+      return false;
+    }
     return true;
-  }else {
+  } else {
     router.navigate(['login']);
     return false;
   }
